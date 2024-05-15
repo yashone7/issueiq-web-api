5a75a2cf5f10b1ac8623ed42b9b77205689c76e9
Yashwanth Somayajula <yashwanth.somayajula16@gmail.com>
2024-05-15 21:35:41 +0530

    added first route

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Hono } from 'hono'
+
+const PORT = process.env.PORT
+
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Bun!'))
+
+export default {
+    port: PORT,
+    fetch: app.fetch
+}
